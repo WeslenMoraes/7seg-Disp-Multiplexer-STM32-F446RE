d317698da2a52a0c1718241446bc6e01934fb3ac
WeslenMoraes <41094032+WeslenMoraes@users.noreply.github.com>
2018-07-17 15:26:00 -0300

    Update code.c

--- code.c
+++ code.c
@@ -1,19 +1,26 @@
+/**
+**===========================================================================
+**
+**  Pinos Utilizados
+**  PA0 = clock do 4017
+**	PB0 = seg a, PB1 = seg b, PB2 = seg c, PB3 = seg d, PB4 = seg e, PB5 = seg f, PB6 = seg g
+**	PC0 = reset do 4017
+**===========================================================================
+*/
 
 /* Includes */
 #include "stm32f4xx.h"
 
-//REVISAR ISSO!(ABCD estão certos)
-
-#define cathode_0                    ((uint32_t)0x03fc)
-#define cathode_1                    ((uint32_t)0X0060)
-#define cathode_2                    ((uint32_t)0X05b0)
-#define cathode_3                    ((uint32_t)0X04f0)
-#define cathode_4                    ((uint32_t)0X0660)
-#define cathode_5                    ((uint32_t)0X06d0)
-#define cathode_6                    ((uint32_t)0X07d0)
-#define cathode_7                    ((uint32_t)0X0070)
-#define cathode_8                    ((uint32_t)0X07fc)
-#define cathode_9                    ((uint32_t)0X06f0)
+#define cathode_0                    ((uint32_t)0x03f)
+#define cathode_1                    ((uint32_t)0X006)
+#define cathode_2                    ((uint32_t)0X05b)
+#define cathode_3                    ((uint32_t)0X04f)
+#define cathode_4                    ((uint32_t)0X066)
+#define cathode_5                    ((uint32_t)0X06d)
+#define cathode_6                    ((uint32_t)0X07d)
+#define cathode_7                    ((uint32_t)0X007)
+#define cathode_8                    ((uint32_t)0X07f)
+#define cathode_9                    ((uint32_t)0X06f)
 #define cathode_A                    ((uint32_t)0X077)
 #define cathode_b                    ((uint32_t)0X07c)
 #define cathode_C                    ((uint32_t)0X039)
@@ -21,10 +28,7 @@
 #define cathode_E                    ((uint32_t)0X07b)
 #define cathode_F                    ((uint32_t)0X071)
 
-#define disp0_ENABLE                 ((uint32_t)0X0400)
-#define disp1_ENABLE                 ((uint32_t)0X1000)
-#define disp2_ENABLE                 ((uint32_t)0X4000)
-#define disp3_ENABLE                 ((uint32_t)0X1000)
+#define TEMPO_TROCA 200
 
 
 /* Private macro */
@@ -42,6 +46,11 @@
 int main(void)
 {
 	uint16_t i = 0;
+	uint16_t k = 0;
+	uint16_t j[] = {cathode_d,cathode_C,cathode_b,cathode_A};
+	uint16_t a[] = {0,0,0,0};
+	uint16_t timerTroca = 0;
+	uint16_t FtimerTroca = 0;
 
 	//Enabling GPIOS clock
 		RCC->AHB1ENR=0X00000087;
@@ -50,15 +59,15 @@ int main(void)
 		RCC->APB2ENR|=RCC_APB2ENR_TIM10EN;
 
 	//Setting UIF overflow time
-		TIM10->PSC = 159;
-		TIM10->ARR = 279;
+		TIM10->PSC = 199;
+		TIM10->ARR = 399;
 		TIM10->CR1|=TIM_CR1_CEN;//starts
 
 	//habilita os pinos do GPIOA como saídas digitais
 		GPIOA->MODER|=0x1;				//controla o contador habilitador dos displays
 
 	//habilita os pinos do GPIOB como saídas digitais
-		GPIOB->MODER=0x5555;			//controla os segmentos dos catodos
+		GPIOB->MODER=0x1555;			//controla os segmentos dos catodos
 
 	//habilita os pinos do GPIOC como saídas digitais
 		GPIOC->MODER=0x1;				//controla o reset
@@ -67,27 +76,26 @@ int main(void)
   while (1)
   {
 
-	  switch(i)
-	  	  {
-	  		  case 0:
-	  			  GPIOC->ODR=0x1;
-	  			  GPIOB->ODR = cathode_d;
-	  		  break;
-	  	  	  case 1:
-	  	  	  	  GPIOB->ODR = cathode_C;
-	  	  	  break;
-	  	  	  case 2:
-	  	  	  	  GPIOB->ODR = cathode_b;
-	  	  	  break;
-	  	  	  case 3:
-	  	  	   	  GPIOB->ODR = cathode_A;
-	  	  	  break;
-	  	  }
+	  if(i == 0){
+		  GPIOC->ODR=0x1;
+		  GPIOB->ODR = j[i];
+	  }
+	  else{
+		  GPIOB->ODR = j[i];
+	  }
 
 	  if(TIM10->SR&TIM_SR_UIF)
 	  	{
 			TIM10->SR  =~ TIM_SR_UIF;	 //Ok, already seen the overflow
 			GPIOC->ODR  = 0x0;
+
+			timerTroca++;
+
+			if(timerTroca == TEMPO_TROCA){
+				timerTroca = 0;
+				FtimerTroca = 1;
+			}
+
 			if (i == 3){
 				i = 0;
 			}
@@ -98,5 +106,20 @@ int main(void)
 			}
 	  	}
 
+	  if(FtimerTroca){
+
+		  FtimerTroca = 0;
+
+		  for(k = 0; k < 4; k++)
+			  a[k] = j[k];
+
+		  j[0] = a[3];
+		  j[1] = a[0];
+		  j[2] = a[1];
+		  j[3] = a[2];
+
+	  }
+
   }//end of while
 }//end of main
+
